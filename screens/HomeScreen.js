2e553d184faacc9aa28e4d84a969fe5451806e7b
Tyler Pritchard <tyler@Tylers-MacBook-Air.local>
2023-06-02 22:21:42 -0700

    Navigation Part 2

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+
+    <View>
+        <Text>Home Screen Text</Text>
+    </View>
+
+}
+
+export default HomeScreen;
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,21 +1,40 @@
 import {Platform, View} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import Constants from 'expo-constants';
-import {createStackNavigator} from '@react-navigation/stack'
 import DirectoryScreen from './DirectoryScreen';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
+import HomeScreen from './HomeScreen';
+
+const Drawer = createDrawerNavigator();
+
+const screenOptions = {
+    headerTintColor: '#fff',
+    headerStyle: { backgroundColor: '#5637DD'}
+}
+
+const HomeNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator
+            screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Home'
+                component={HomeScreen}
+                options={{title: 'Home'}}
+            />
+        </Stack.Navigator>
+    )
+}
 
 const DirectoryNavigator = () => {
-    const Stack = createStackNavigator()
+    const Stack = createStackNavigator();
 
     return (
         <Stack.Navigator
             initialRouteName = 'Directory'
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#5637DD'
-                },
-                headerTintColor: '#fff'
-            }} 
+            screenOptions={screenOptions} 
         >
             <Stack.Screen 
                 name='CampsiteInfo'
@@ -30,8 +49,8 @@ const DirectoryNavigator = () => {
                 options={{title: 'Campsite Directory'}}
             />
         </Stack.Navigator>
-    )
-}
+    );
+};
 
 const Main = () => {
 
@@ -41,10 +60,25 @@ const Main = () => {
             paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
  
         }}>
-            <DirectoryNavigator />
+            <Drawer.Navigator
+                initialRouteName='Home'
+                drawerStyle={{
+                    backgroundColor: '#CEC8FF'
+                }}
+            >
+                <Drawer.Screen 
+                    name='Home'
+                    component={HomeNavigator}
+                    options={{ title: 'Home' }}
+                />
+                <Drawer.Screen 
+                    name='Directory'
+                    component={DirectoryNavigator}
+                    options={{ title: 'Directory' }}
+                />
+            </Drawer.Navigator>
         </View>
     )
-
 }
 
 export default Main;

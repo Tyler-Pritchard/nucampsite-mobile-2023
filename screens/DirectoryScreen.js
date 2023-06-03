d7b70ce0c3a9ff24a008f108b669276e30eae3a6
Tyler Pritchard <tyler@Tylers-MacBook-Air.local>
2023-06-02 20:54:34 -0700

    componenets 2

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -5,15 +5,15 @@ const DirectoryScreen = (props) => {
 
     const renderDirectoryItem = ({item: campsite}) => {
         return (
-            <ListItem>
+            <ListItem onPress={() => props.onPress(campsite.id)}>
                 <Avatar source={campsite.image} rounded />
                 <ListItem.Content>
                     <ListItem.Title>{campsite.name}</ListItem.Title>
                     <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
                 </ListItem.Content>
             </ListItem>
-        )
-    }
+        );
+    };
 
     return <FlatList 
         data = {props.campsites}
